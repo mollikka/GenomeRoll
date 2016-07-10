18f35be32acc83fec93eeb49c448865f872bf8ee
Lauri Tervonen <lauri.tervonen@aalto.fi>
2016-07-11 02:59:03 +0300

    minify js on compile

--- compile.sh
+++ compile.sh
@@ -1,6 +1,10 @@
-rm -r compiled
 mkdir compiled
+rm compiled/* -r
+
 cp index.html compiled/
 cp main.css compiled/
 cp fantasydog.json compiled/
+
 cat *.js > compiled/breeding.js
+curl -X POST -s --data-urlencode 'input@compiled/breeding.js' https://javascript-minifier.com/raw > compiled/breeding.min.js
+rm compiled/breeding.js
